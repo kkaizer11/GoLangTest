0ecf34721dc5909453f4acfd6630cc476b284797
kkaizer11 <kaizerin.m@gmail.com>
2023-10-31 20:29:41 -0300

    adicionando um todo no bubble sort

--- 42 examples/bubble.go
+++ 42 examples/bubble.go
@@ -15,16 +15,18 @@ func ft_bubble_sort(tab []int) []int {
 		size = len(tab)
 	)
 	for i < size {
-		i++ //pode ser assim
+		i++ // pode ser assim
 		for j < size-1-i {
 			if tab[j] > tab[j+1] {
 				ft_swap(&tab[j], &tab[j+1])
 			}
-			j += 1 //ou assim
+			j += 1 // ou assim
 		}
 	}
 	return tab
 }
+
+// TODO: Adicionar de fato a funçao random
 
 // func main() {
 // 	rand_num := []int{90, -5, 13, 58, 27}
