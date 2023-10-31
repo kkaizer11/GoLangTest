38c25b6c8e12bebe5a8be4016e1867a627d6dc8e
kkaizer11 <kaizerin.m@gmail.com>
2023-10-31 15:50:15 -0300

    atualizando

--- 42 examples/bubble.go
+++ 42 examples/bubble.go
@@ -1,34 +1,33 @@
-package main 
+package main
 
 import "fmt"
 
-func ft_swap(a *int ,b *int){
-  tmp := *a
-  *a = *b
-  *b = tmp
+func ft_swap(a *int, b *int) {
+	tmp := *a
+	*a = *b
+	*b = tmp
 }
 
-func ft_bubble_sort(tab []int) []int{
-  var(
-    i = 0
-    j = 0
-    size = len(tab)
-  )
-  for i < size{
-    i += 1
-    for j < size-1-i {
-      if tab[j] > tab[j+1] {
-        ft_swap(&tab[j], &tab[j+1])
-      }
-      j += 1
-    }
-  }
-  return tab
+func ft_bubble_sort(tab []int) []int {
+	var (
+		i    = 0
+		j    = 0
+		size = len(tab)
+	)
+	for i < size {
+		i++ //pode ser assim
+		for j < size-1-i {
+			if tab[j] > tab[j+1] {
+				ft_swap(&tab[j], &tab[j+1])
+			}
+			j += 1 //ou assim
+		}
+	}
+	return tab
 }
 
-
-func main(){
-  rand_num := []int{90, -5 , 13, 58,27}
-  ft_bubble_sort(rand_num)
-  fmt.Println(rand_num)
+func main() {
+	rand_num := []int{90, -5, 13, 58, 27}
+	ft_bubble_sort(rand_num)
+	fmt.Println(rand_num)
 }
--- func1.go
+++
@@ -1,7 +0,0 @@
-package "lib"
-
-import "fmt"
-
-func func1()  {
-  fmt.Println("importado")
-}
