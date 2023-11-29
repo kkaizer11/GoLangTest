af147c4e66a52faeb8dc37e45370a1f76d6e3421
kkaizer11 <kaizerin.m@gmail.com>
2023-11-29 19:57:32 -0300

    finalizei o que me interessa

--- bootdev/15 - generics/1-generics_in_go.go
+++ bootdev/15 - generics/1-generics_in_go.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
-func getLast[]() {
-
+func getLast[T any](s []T) T {
+	if len(s) == 0 {
+		var x T
+		return x
+	}
+	return s[len(s)-1]
 }
 
 // don't edit below this line
@@ -72,4 +76,3 @@ func test[T any](s []T, desc string) {
 	fmt.Printf("Last item in list: %v\n", last)
 	fmt.Println(" --- ")
 }
-
