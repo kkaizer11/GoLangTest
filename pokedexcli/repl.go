34f0ea9f5aa37e3692c0912f050563d6b9e116fd
kkaizer11 <kaizerin.m@gmail.com>
2023-12-03 00:24:34 -0300

    Invalid command
    Apenas completando a mensage de ajuda para quando o um comando invalido é digitado

--- pokedexcli/repl.go
+++ pokedexcli/repl.go
@@ -20,7 +20,7 @@ func startRepl() {
 		commandName := cleanned[0]
 		command, ok := available_commands[commandName]
 		if !ok {
-			fmt.Println("Invalid Command")
+			fmt.Println("Invalid Command, type \"help\" to display all availabe commands")
 			continue
 		}
 		command.callback()
